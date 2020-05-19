59995d1b97af9e875caa6aa9dfd72a8159fde213
Omodaka9375 <branislav.djalic@gmail.com>
2020-05-19 02:05:50 +0200

    initial commit

---
+++ src/helpers/Constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+  public static stsAuthority = 'https://api.vivokey.com/openid/';
+  public static clientId = '<YOUR-CLIENT-ID>';
+  public static clientSecret = '<YOUR-CLIENT-SECRET>';
+  public static clientRoot = 'http://localhost:4200/';
+  public static clientScope = 'openid profile email api';
+  public static apiRoot = 'https://api.vivokey.com/';
+  public static iftttSecretKey = '<YOUR-IFTT-CLIENT-SECRET>';
+  public static iftttEvent = '<IFTTT-EVENT-NAME>';
+}
